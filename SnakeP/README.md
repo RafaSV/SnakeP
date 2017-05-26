605101d0c4aceeb9c1cf28a4047054e4a6054514
RafaSV <rafaast@outlook.com>
2017-05-26 10:11:45 -0300

    For sure

---
+++ SnakeP/README.md
@@ -0,0 +1,2 @@
+# SnakeP
+Nagini
---
+++ SnakeP/sketch.js
@@ -0,0 +1,29 @@
+var s;
+var scl = 20;
+
+function setup() {
+  createCanvas(600, 600);
+  s = new Snake();
+  frameRate(10);
+}
+
+function draw(){
+  background(50);
+  s.update();
+  s.show();
+}
+
+function keyPressed() {
+  if (keyCode === UP_ARROW) {
+    s.dir(0,-1);
+  }
+  else if (keyCode === DOWN_ARROW) {
+    s.dir(0, 1);
+  }
+  else if (keyCode === RIGHT_ARROW) {
+    s.dir(1, 0);
+  }
+  else if (keyCode === LEFT_ARROW) {
+    s.dir(-1, 0);
+  }
+}
